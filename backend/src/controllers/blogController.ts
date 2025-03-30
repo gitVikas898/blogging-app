5336ba27f7b5c3df288cf89e1d58115a37cd2bea
Vikas <vikas.kumar.singh1198@gmail.com>
2025-03-30 14:21:23 +0530

    wrote blog controllers

---
+++ backend/src/controllers/blogController.ts
@@ -0,0 +1,42 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Request,Response} from "express"
+const prisma = new PrismaClient();
+
+interface BlogRequestBody{
+    title:string;
+    content:string;
+}
+
+interface AuthenticatedRequest extends Request{
+    user?:{userId:number };
+}
+
+async function createBlog(req:AuthenticatedRequest,res:Response):Promise<void> {
+    const {title,content}:BlogRequestBody = req.body;
+    if(!req.user){
+        res.status(401).json({message:"Unauthorised"});
+        return;
+    }
+
+    try{
+        const blog = await prisma.blog.create({
+            data:{title,content,authorId:req.user.userId}
+        })
+        res.status(201).json(blog);
+    }catch(error){
+        res.status(500).json({ message: (error as Error).message });
+    }
+}
+
+async function getBlogs(req:Request,res:Response) {
+    try{
+        const blogs = await prisma.blog.findMany({
+            include:{
+                author:{select:{username:true}}
+            }
+        })
+        res.status(200).json(blogs);
+    }catch(error){
+        res.status(500).json({ message: (error as Error).message });
+    }
+}
